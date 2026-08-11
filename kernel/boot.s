c976f3856c4718d517fde43a0869a863e332d761
Diego Araujo <dweg0epic@outlook.com>
2026-08-10 21:13:18 -0300

    feat(kernel): add memory and state initialization

--- kernel/boot.s
+++ kernel/boot.s
@@ -3,6 +3,9 @@
 .thumb
 
 .extern kernel_main
+.extern initialize_data
+.extern initialize_bss
+
 .extern nmi_handler
 .extern hard_fault_handler
 .extern default_handler
@@ -11,8 +14,9 @@
 .balign 4
 
 .global vector_table
+
 vector_table:
-    .word 0x20005000          @ Initial MSP
+    .word _stack_top          @ Initial MSP
     .word reset_handler       @ Reset
     .word nmi_handler         @ NMI
     .word hard_fault_handler  @ HardFault
@@ -31,13 +35,20 @@ vector_table:
     .word default_handler     @ PendSV
     .word default_handler     @ SysTick
 
+
 .section .text.reset_handler, "ax", %progbits
+.balign 2
+
 .global reset_handler
 .type reset_handler, %function
 .thumb_func
 
 reset_handler:
+    bl initialize_data
+    bl initialize_bss
     bl kernel_main
 
 reset_hang:
     b reset_hang
+
+.size reset_handler, . - reset_handler
